6cd1b18650fbd842430ee49c372a8dfef919639c
Isotarge <isaacjmiell@gmail.com>
2015-10-03 11:24:57 +0930

    Initial commit
    Adding what I have so far

---
+++ RomThing/RomThing.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace RomThing
+{
+    partial class RomThing
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RomThing));
+			this.txtROMDirectory = new System.Windows.Forms.TextBox();
+			this.btnOpenDirectory = new System.Windows.Forms.Button();
+			this.filterCheckList = new System.Windows.Forms.CheckedListBox();
+			this.btnApplyFilters = new System.Windows.Forms.Button();
+			this.listROMs = new System.Windows.Forms.CheckedListBox();
+			this.btnDeleteROMs = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// txtROMDirectory
+			// 
+			this.txtROMDirectory.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtROMDirectory.Enabled = false;
+			this.txtROMDirectory.Location = new System.Drawing.Point(12, 12);
+			this.txtROMDirectory.Name = "txtROMDirectory";
+			this.txtROMDirectory.ReadOnly = true;
+			this.txtROMDirectory.Size = new System.Drawing.Size(423, 20);
+			this.txtROMDirectory.TabIndex = 2;
+			// 
+			// btnOpenDirectory
+			// 
+			this.btnOpenDirectory.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnOpenDirectory.Location = new System.Drawing.Point(441, 12);
+			this.btnOpenDirectory.Name = "btnOpenDirectory";
+			this.btnOpenDirectory.Size = new System.Drawing.Size(111, 20);
+			this.btnOpenDirectory.TabIndex = 4;
+			this.btnOpenDirectory.Text = "Choose Directory";
+			this.btnOpenDirectory.UseVisualStyleBackColor = true;
+			this.btnOpenDirectory.Click += new System.EventHandler(this.btnOpenDirectory_Click);
+			// 
+			// filterCheckList
+			// 
+			this.filterCheckList.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.filterCheckList.FormattingEnabled = true;
+			this.filterCheckList.HorizontalScrollbar = true;
+			this.filterCheckList.IntegralHeight = false;
+			this.filterCheckList.Location = new System.Drawing.Point(363, 38);
+			this.filterCheckList.Name = "filterCheckList";
+			this.filterCheckList.SelectionMode = System.Windows.Forms.SelectionMode.None;
+			this.filterCheckList.Size = new System.Drawing.Size(189, 193);
+			this.filterCheckList.TabIndex = 5;
+			// 
+			// btnApplyFilters
+			// 
+			this.btnApplyFilters.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnApplyFilters.Enabled = false;
+			this.btnApplyFilters.Location = new System.Drawing.Point(363, 237);
+			this.btnApplyFilters.Name = "btnApplyFilters";
+			this.btnApplyFilters.Size = new System.Drawing.Size(189, 33);
+			this.btnApplyFilters.TabIndex = 6;
+			this.btnApplyFilters.Text = "Apply Filters";
+			this.btnApplyFilters.UseVisualStyleBackColor = true;
+			this.btnApplyFilters.Click += new System.EventHandler(this.btnApplyFilters_Click);
+			// 
+			// listROMs
+			// 
+			this.listROMs.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.listROMs.FormattingEnabled = true;
+			this.listROMs.HorizontalScrollbar = true;
+			this.listROMs.IntegralHeight = false;
+			this.listROMs.Location = new System.Drawing.Point(12, 38);
+			this.listROMs.Name = "listROMs";
+			this.listROMs.Size = new System.Drawing.Size(345, 271);
+			this.listROMs.TabIndex = 7;
+			// 
+			// btnDeleteROMs
+			// 
+			this.btnDeleteROMs.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnDeleteROMs.Enabled = false;
+			this.btnDeleteROMs.Location = new System.Drawing.Point(363, 276);
+			this.btnDeleteROMs.Name = "btnDeleteROMs";
+			this.btnDeleteROMs.Size = new System.Drawing.Size(189, 33);
+			this.btnDeleteROMs.TabIndex = 8;
+			this.btnDeleteROMs.Text = "Delete ROMs";
+			this.btnDeleteROMs.UseVisualStyleBackColor = true;
+			this.btnDeleteROMs.Click += new System.EventHandler(this.btnDeleteROMs_Click);
+			// 
+			// RomThing
+			// 
+			this.AcceptButton = this.btnOpenDirectory;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(564, 321);
+			this.Controls.Add(this.btnDeleteROMs);
+			this.Controls.Add(this.listROMs);
+			this.Controls.Add(this.btnApplyFilters);
+			this.Controls.Add(this.filterCheckList);
+			this.Controls.Add(this.btnOpenDirectory);
+			this.Controls.Add(this.txtROMDirectory);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MinimumSize = new System.Drawing.Size(580, 360);
+			this.Name = "RomThing";
+			this.Text = "ROMThing";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtROMDirectory;
+        private System.Windows.Forms.Button btnOpenDirectory;
+		private System.Windows.Forms.CheckedListBox filterCheckList;
+		private System.Windows.Forms.Button btnApplyFilters;
+		private System.Windows.Forms.CheckedListBox listROMs;
+		private System.Windows.Forms.Button btnDeleteROMs;
+	}
+}
+
---
+++ RomThing/RomThing.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RomThing {
+	public partial class RomThing : Form {
+		public string ROMPath;
+		public bool PathChosen = false;
+		public List<string> ROMs;
+		public List<string> filters;
+		public string FILTER_PATH = "config.txt";
+
+		public RomThing() {
+			InitializeComponent();
+		}
+
+		private void populateROMList() {
+			if (PathChosen) {
+				ROMs = Directory.EnumerateFiles(ROMPath).ToList();
+				listROMs.Items.Clear();
+				for (var i = 0; i < ROMs.Count(); i++) {
+					ROMs[i] = Path.GetFileName(ROMs[i]);
+					listROMs.Items.Add(ROMs[i]);
+				}
+			}
+		}
+
+		private void applyFilters() {
+			if (PathChosen) {
+				for (int i = 0; i < listROMs.Items.Count; i++) {
+					listROMs.SetItemChecked(i, false);
+					foreach (string filter in filterCheckList.CheckedItems) {
+						if (ROMs[i].Contains(filter)) {
+							listROMs.SetItemChecked(i, true);
+							break;
+						}
+					}
+				}
+			}
+		}
+
+		private void Form1_Load(object sender, EventArgs e) {
+			filters = new List<string>();
+			if (File.Exists(FILTER_PATH)) {
+				var filterFile = File.ReadAllLines(FILTER_PATH);
+				foreach(string line in filterFile) {
+					if (line.StartsWith(";") || line.Trim() == String.Empty) {
+						// Ignore line
+					} else {
+						filters.Add(line);
+					}
+				}
+			} else {
+				MessageBox.Show(FILTER_PATH + " was not found in the application directory, using default filters...", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+				// Bad dumps
+				filters.Add("[b1");
+				filters.Add("[b2");
+				filters.Add("[b3");
+				filters.Add("[b4");
+				filters.Add("[b5");
+				filters.Add("[b6");
+				filters.Add("[b7");
+				filters.Add("[b8");
+				filters.Add("[b9");
+				filters.Add("[bI");
+
+				// Hacked (intros etc)
+				filters.Add("[h1");
+				filters.Add("[h2");
+				filters.Add("[h3");
+				filters.Add("[h4");
+				filters.Add("[h5");
+				filters.Add("[h6");
+				filters.Add("[h7");
+				filters.Add("[h8");
+				filters.Add("[h9");
+				filters.Add("[hI");
+
+				// "Fixed"
+				filters.Add("[f1");
+				filters.Add("[f2");
+				filters.Add("[f3");
+				filters.Add("[f4");
+				filters.Add("[f5");
+				filters.Add("[f6");
+				filters.Add("[f7");
+				filters.Add("[f8");
+				filters.Add("[f9");
+				filters.Add("[fI");
+
+				// Overdumps
+				filters.Add("[o1");
+				filters.Add("[o2");
+				filters.Add("[o3");
+				filters.Add("[o4");
+				filters.Add("[o5");
+				filters.Add("[o6");
+				filters.Add("[o7");
+				filters.Add("[o8");
+				filters.Add("[o9");
+
+				// Pirated
+				filters.Add("[p1");
+				filters.Add("[p2");
+				filters.Add("[p3");
+				filters.Add("[p4");
+				filters.Add("[p5");
+				filters.Add("[p6");
+				filters.Add("[p7");
+				filters.Add("[p8");
+				filters.Add("[p9");
+
+				// Trainers
+				filters.Add("[t1");
+				filters.Add("[t2");
+				filters.Add("[t3");
+				filters.Add("[t4");
+				filters.Add("[t5");
+				filters.Add("[t6");
+				filters.Add("[t7");
+				filters.Add("[t8");
+				filters.Add("[t9");
+
+				// Non-English translations
+				filters.Add("[T+Bra");
+				filters.Add("[T+Chi");
+				filters.Add("[T+Dan");
+				filters.Add("[T+Fin");
+				filters.Add("[T+Fre");
+                filters.Add("[T+Ger");
+				filters.Add("[T+Ita");
+				filters.Add("[T+Kor");
+				filters.Add("[T+Lat");
+				filters.Add("[T+Nor");
+				filters.Add("[T+Pol");
+				filters.Add("[T+Por");
+				filters.Add("[T+Rus");
+				filters.Add("[T+Spa");
+				filters.Add("[T+Swe");
+				filters.Add("[T+Spa");
+				filters.Add("[T+Thai");
+
+				// Unfinished non-English translations
+				filters.Add("[T-Bra");
+				filters.Add("[T-Chi");
+				filters.Add("[T-Dan");
+				filters.Add("[T-Fin");
+				filters.Add("[T-Fre");
+				filters.Add("[T-Ger");
+				filters.Add("[T-Ita");
+				filters.Add("[T-Kor");
+				filters.Add("[T-Lat");
+				filters.Add("[T-Nor");
+				filters.Add("[T-Pol");
+				filters.Add("[T-Por");
+				filters.Add("[T-Rus");
+				filters.Add("[T-Spa");
+				filters.Add("[T-Swe");
+				filters.Add("[T-Spa");
+				filters.Add("[T-Thai");
+			}
+			foreach (string filter in filters) {
+				filterCheckList.Items.Add(filter);
+			}
+			for (var i = 0; i < filterCheckList.Items.Count; i++) {
+				filterCheckList.SetItemChecked(i, true);
+			}
+		}
+
+		private void btnOpenDirectory_Click(object sender, EventArgs e) {
+			FolderBrowserDialog dlgOpenDirectory = new FolderBrowserDialog();
+			if (dlgOpenDirectory.ShowDialog() == DialogResult.OK) {
+				PathChosen = true;
+				ROMPath = dlgOpenDirectory.SelectedPath;
+				txtROMDirectory.Text = ROMPath;
+
+				populateROMList();
+				applyFilters();
+
+				// Update buttons
+				btnApplyFilters.Enabled = true;
+				btnDeleteROMs.Enabled = true;
+				this.AcceptButton = btnDeleteROMs;
+			}
+		}
+
+		private void btnApplyFilters_Click(object sender, EventArgs e) {
+			applyFilters();
+		}
+
+		private void btnDeleteROMs_Click(object sender, EventArgs e) {
+			if (listROMs.CheckedItems.Count > 0) {
+				var result = MessageBox.Show("This will permanently delete the " + listROMs.CheckedItems.Count + " selected files, are you sure you wish to do this?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+				if (result == DialogResult.Yes) {
+					foreach (string ROMtoDelete in listROMs.CheckedItems) {
+						File.Delete(ROMPath + "\\" + ROMtoDelete);
+					}
+					populateROMList();
+					applyFilters();
+				}
+			} else {
+				MessageBox.Show("There are no ROMs selected to delete.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			}
+		}
+	}
+}
